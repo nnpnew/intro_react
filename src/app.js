b8c063a588dbac1030b6c642499a3f913f9509e6
CSTURoom107 <CSTURoom107>
2016-10-29 12:08:40 +0700

    add movie detail

--- src/app.js
+++ src/app.js
@@ -2,22 +2,83 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {SearchForm} from './search-form'
 import axios from 'axios'
+import {
+    Router,
+    Route,
+    hashHistory,
+    Link,
+    IndexRoute
+} from 'react-router'
+
+const batmanQuery ={
+    pathname: '/search',
+    query: {
+        s: 'batman'
+    }
+}
+
+const avengersQuery={
+    pathname: '/search',
+    query:{
+        s: 'avengers'
+    }
+}
+const doctorStrageQuery={
+    pathname: '/search',
+    query:{
+        s: 'doctor strange'
+    }
+}
+const Home = () =>(
+
+    <section>
+        <h1>This is Home</h1>
+        <ul>
+            <li><Link to={batmanQuery}>Batman</Link></li>
+            <li><Link to={avengersQuery}>Avengers</Link></li>
+            <li><Link to={doctorStrageQuery}>Doctor Strange</Link></li>
+        </ul>
+    </section>
+)
+
+const Nav = () =>(
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search</Link></li>
+            <li><Link to="/detail">Detail</Link></li>
+        </ul>
+    </nav>
+)
 
 const MovieList = (props) => (
     <ul>
     {props.movies.map((movie, i) => {
+        const query = {
+            pathname : '/detail',
+            query:{
+                id: movie.imdbID
+            }
+        }
         return (
-            <li key={i}>{movie.Title}</li>
+            <li key={i}>
+                <h4><Link to={query}>{movie.Title}</Link></h4>
+                <img src={movie.Poster}/>
+            </li>
         )
     })}
     </ul>
 )
 
-class App extends React.Component{
+class Search extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             movies: []
+        }
+        if(props.location.query.s){
+        this.onSearch(props.location.query.s)
+        //console.log('query',props.location.query)
         }
     }
     onSearch(query){
@@ -40,4 +101,62 @@ class App extends React.Component{
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const App = props => (
+    <section>
+        <Nav />
+        {props.children}
+    </section>
+)
+
+class MovieDetail extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            movie: {
+                Title: 'unknown'
+            }
+        }
+        if(props.location.query.id){
+            const id = props.location.query.id
+            axios.get(`http://www.omdbapi.com/?i=${id}&plot=short&r=json`)
+            .then(response=>{
+                const movie = response.data
+                this.setState({
+                    movie: movie
+                })
+            })
+        }
+    }
+    render(){
+        const {
+            Title,
+            Genre,
+            Poster
+        }= this.state.movie
+        return(
+            <section>
+                <h1>{Title}</h1>
+                <small>{Genre}</small>
+                <div>
+                    <img src={Poster}/>
+                </div>
+            </section>
+        )
+    }
+}
+
+class Main extends React.Component{
+    render(){
+        return (
+            <Router history = {hashHistory}>
+                <Route path="/" component={App} >
+                    <IndexRoute component={Home}/>
+                    <Route path="/search" component={Search}/>
+                    <Route path="/detail" component={MovieDetail}/>
+
+                </Route>
+            </Router>
+        )
+    }
+}
+ReactDOM.render(<Main />, document.getElementById('app'))
